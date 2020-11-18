a9dc6e7a8224fc8e5982a5fea8635649b5d891cd
Nurverek <nurverek@gmail.com>
2020-11-18 18:13:18 +0330

    Updated FSLink to be abstract, links must define themselves

--- firestorm/src/main/java/com/nurverek/firestorm/FSLink.java
+++ firestorm/src/main/java/com/nurverek/firestorm/FSLink.java
@@ -3,33 +3,18 @@ package com.nurverek.firestorm;
 import com.nurverek.vanguard.VLBuffer;
 import com.nurverek.vanguard.VLBufferable;
 
-public class FSLink<LINK, BUFFERTYPE extends VLBuffer> implements VLBufferable<BUFFERTYPE>{
+public abstract class FSLink<LINK, BUFFERTYPE extends VLBuffer, CONFIG extends FSConfig> implements VLBufferable<BUFFERTYPE>{
 
     public LINK link;
-    public FSConfig config;
+    public CONFIG config;
     public FSBufferAddress address;
 
-    public FSLink(LINK link, FSConfig config){
+    public FSLink(LINK link, CONFIG config){
         this.link = link;
         this.config = config;
     }
 
     public FSLink(LINK link){
         this.link = link;
-    }
-
-    @Override
-    public void buffer(BUFFERTYPE buffertype){
-
-    }
-
-    @Override
-    public void buffer(BUFFERTYPE buffertype, int i, int i1){
-
-    }
-
-    @Override
-    public void buffer(BUFFERTYPE buffertype, int i, int i1, int i2, int i3, int i4, int i5){
-
     }
 }
