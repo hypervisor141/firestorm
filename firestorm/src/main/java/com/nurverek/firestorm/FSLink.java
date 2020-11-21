77c99f92c291512771a5cd370a4e5d5df6d1e214
Nurverek <nurverek@gmail.com>
2020-11-21 13:43:58 +0330

    LINK type and reference was removed in FSLink as it was not needed

--- firestorm/src/main/java/com/nurverek/firestorm/FSLink.java
+++ firestorm/src/main/java/com/nurverek/firestorm/FSLink.java
@@ -4,27 +4,21 @@ import com.nurverek.vanguard.VLBufferAddress;
 import com.nurverek.vanguard.VLBufferManagerBase;
 import com.nurverek.vanguard.VLBufferable;
 
-public abstract class FSLink<LINK, CONFIG extends FSConfig, ENTRYTYPE extends VLBufferManagerBase.EntryType,
+public abstract class FSLink<CONFIG extends FSConfig, ENTRYTYPE extends VLBufferManagerBase.EntryType,
         MANAGERTYPE extends VLBufferManagerBase, ADDRESSTYPE extends VLBufferAddress<MANAGERTYPE>>
         implements VLBufferable<ENTRYTYPE, MANAGERTYPE, ADDRESSTYPE>{
 
-    public LINK link;
     public CONFIG config;
     public FSConfigDynamic<CONFIG> host;
     public FSBufferAddress address;
 
-    public FSLink(LINK link, CONFIG config, FSConfigDynamic host){
-        this.link = link;
+    public FSLink(CONFIG config, FSConfigDynamic host){
         this.config = config;
         this.host = host;
     }
 
     public FSLink(){
-        
-    }
 
-    public FSLink(LINK link){
-        this.link = link;
     }
 
     public void attach(){
