ee856e596a7a17996eb350fa31a367467bccf3ef
Nurverek <nurverek@gmail.com>
2020-12-28 16:30:39 +0330

    Fixed a debugging issue

--- firestorm/src/main/java/com/nurverek/firestorm/FSBufferAddress.java
+++ firestorm/src/main/java/com/nurverek/firestorm/FSBufferAddress.java
@@ -23,8 +23,31 @@ public class FSBufferAddress extends VLBufferAddress<FSBufferManager>{
     public void bind(){
         target().vertexBuffer().bind();
     }
-
+    
     public void unbind(){
         target().vertexBuffer().unbind();
+    }
+
+    @Override
+    public void stringify(StringBuilder src, Object hint){
+        FSVertexBuffer buffer = target().vertexBuffer();
+
+        src.append("[BufferAddress] managerType[");
+        src.append(manager.getClass().getSimpleName());
+        src.append("] offset[");
+        src.append(offset);
+        src.append("] unitSize[");
+        src.append(unitsize);
+        src.append("] unitOffset[");
+        src.append(unitoffset);
+        src.append("] stride[");
+        src.append(stride);
+        src.append("] count[");
+        src.append(count);
+        src.append("] bindPoint[");
+        src.append(buffer.bindPoint());
+        src.append("] content[ ");
+        buffer.provider().stringify(src, hint);
+        src.append(" ]");
     }
 }
