f8faea867588c8ea5a77e0b8166e8dc7b071d217
Nurverek <nurverek@gmail.com>
2021-03-10 09:56:59 +0330

    FS now provides only one detailed buffer type backed by FSVertexBuffer

--- firestorm/src/main/java/com/nurverek/firestorm/FSBufferTracker.java
+++ firestorm/src/main/java/com/nurverek/firestorm/FSBufferTracker.java
@@ -1,24 +1,89 @@
 package com.nurverek.firestorm;
 
-import vanguard.VLBuffer;
-import vanguard.VLBufferTracker;
+import vanguard.VLBufferTrackerType;
 
-public class FSBufferTracker<BUFFER extends VLBuffer<?, ?>> extends VLBufferTracker<BUFFER>{
+public class FSBufferTracker<BUFFER extends FSVertexBuffer<?>> extends VLBufferTrackerType<BUFFER>{
 
-    public FSVertexBuffer<BUFFER> buffer;
+    protected int inputoffest;
+    protected int unitoffset;
+    protected int unitsize;
+    protected int unitsubcount;
+    protected int stride;
 
-    public FSBufferTracker(FSVertexBuffer<BUFFER> buffer, int offset, int count){
-        super(buffer.provider(), offset, count);
-        this.buffer = buffer;
+    public FSBufferTracker(BUFFER buffer, int offset, int inputoffest, int count, int unitoffset, int unitsize, int unitsubcount, int stride){
+        super(buffer, offset, count);
+
+        this.inputoffest = inputoffest;
+        this.unitoffset = unitoffset;
+        this.unitsize = unitsize;
+        this.unitsubcount = unitsubcount;
+        this.stride = stride;
     }
 
     public FSBufferTracker(){
-
     }
 
-    @Override
-    public void buffer(BUFFER buffer){
-        super.buffer(buffer);
-        this.buffer.provider(buffer);
+    public void inputoffest(int inputoffest){
+        this.inputoffest = inputoffest;
+    }
+
+    public void unitoffset(int unitoffset){
+        this.unitoffset = unitoffset;
+    }
+
+    public void unitsize(int unitsize){
+        this.unitsize = unitsize;
+    }
+
+    public void unitsubcount(int unitsubcount){
+        this.unitsubcount = unitsubcount;
+    }
+
+    public void stride(int stride){
+        this.stride = stride;
+    }
+
+    public int inputoffest(){
+        return this.inputoffest;
+    }
+
+    public int unitoffset(){
+        return this.unitoffset;
+    }
+
+    public int unitsize(){
+        return this.unitsize;
+    }
+
+    public int unitsubcount(){
+        return this.unitsubcount;
+    }
+
+    public int stride(){
+        return this.stride;
+    }
+
+    public void stringify(StringBuilder src, Object hint){
+        src.append("[");
+        src.append(getClass().getSimpleName());
+        src.append("] buffer[");
+        src.append(buffer.getClass().getSimpleName());
+        src.append("] offset[");
+        src.append(offset);
+        src.append("] count[");
+        src.append(count);
+        src.append("] unitOffset[");
+        src.append(unitoffset);
+        src.append("] unitSize[");
+        src.append(unitsize);
+        src.append("] unitSubCount[");
+        src.append(unitsubcount);
+        src.append("] stride[");
+        src.append(stride);
+        src.append("] content[ ");
+        
+        buffer.stringify(src, hint);
+        
+        src.append(" ]");
     }
 }
