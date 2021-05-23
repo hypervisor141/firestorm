193d397b87029192bf4b0b19e44096907d6a6575
Nurverek <nurverek@gmail.com>
2021-05-23 13:57:41 +0430

    Added more modifications for custom assembler

--- firestorm/src/main/java/com/nurverek/firestorm/FSHAssembler.java
+++ firestorm/src/main/java/com/nurverek/firestorm/FSHAssembler.java
@@ -29,19 +29,19 @@ public abstract class FSHAssembler implements VLLoggable{
     public boolean CONVERT_POSITIONS_TO_MODELARRAYS = false;
     public boolean DRAW_MODE_INDEXED = false;
 
-    private final VLListType<BuildStep> firstfuncs;
-    private final VLListType<BuildStep> restfuncs;
-    private final VLListType<BuildStep> customfunc;
+    private final VLListType<BuildStep> firstInstanceSteps;
+    private final VLListType<BuildStep> otherInstanceSteps;
+    private final VLListType<BuildStep> customInstanceSteps;
 
     public FSHAssembler(int customstepcapacity){
-        firstfuncs = new VLListType<>(10, 20);
-        restfuncs = new VLListType<>(10, 20);
-        customfunc = new VLListType<>(customstepcapacity, customstepcapacity);
+        firstInstanceSteps = new VLListType<>(10, 20);
+        otherInstanceSteps = new VLListType<>(10, 20);
+        customInstanceSteps = new VLListType<>(customstepcapacity, customstepcapacity);
 
         setDefaultAll();
     }
 
-    protected abstract void setup();
+    protected abstract void setup(VLListType<BuildStep> firstInstanceSteps, VLListType<BuildStep> otherInstanceSteps, VLListType<BuildStep> customInstanceSteps);
 
     public void setDefaultAll(){
         LOAD_MODELS = true;
@@ -65,10 +65,11 @@ public abstract class FSHAssembler implements VLLoggable{
     }
 
     public void configure(){
-        setup();
+        otherInstanceSteps.clear();
+        firstInstanceSteps.clear();
+        customInstanceSteps.clear();
 
-        restfuncs.clear();
-        firstfuncs.clear();
+        setup(firstInstanceSteps, otherInstanceSteps, customInstanceSteps);
 
         configureModels();
         configurePositions();
@@ -79,55 +80,55 @@ public abstract class FSHAssembler implements VLLoggable{
     }
 
     public VLListType<BuildStep> customSteps(){
-        return customfunc;
+        return customInstanceSteps;
     }
 
     private void configureModels(){
         if(LOAD_MODELS){
-            firstfuncs.add(MODEL_INITIALIZE);
-            restfuncs.add(MODEL_INITIALIZE);
+            firstInstanceSteps.add(MODEL_INITIALIZE);
+            otherInstanceSteps.add(MODEL_INITIALIZE);
         }
     }
 
     private void configurePositions(){
         if(LOAD_POSITIONS){
-            firstfuncs.add(POSITION_SET);
+            firstInstanceSteps.add(POSITION_SET);
 
             if(!DRAW_MODE_INDEXED){
-                firstfuncs.add(POSITION_UNINDEX);
+                firstInstanceSteps.add(POSITION_UNINDEX);
             }
 
-            firstfuncs.add(POSITION_INIT_SCHEMATICS);
+            firstInstanceSteps.add(POSITION_INIT_SCHEMATICS);
 
             if(CONVERT_POSITIONS_TO_MODELARRAYS){
-                firstfuncs.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
+                firstInstanceSteps.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
             }
 
             if(INSTANCE_SHARE_POSITIONS){
                 if(CONVERT_POSITIONS_TO_MODELARRAYS){
-                    restfuncs.add(POSITION_SET);
+                    otherInstanceSteps.add(POSITION_SET);
 
                     if(!DRAW_MODE_INDEXED){
-                        restfuncs.add(POSITION_UNINDEX);
+                        otherInstanceSteps.add(POSITION_UNINDEX);
                     }
 
-                    restfuncs.add(POSITION_INIT_SCHEMATICS);
-                    restfuncs.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
+                    otherInstanceSteps.add(POSITION_INIT_SCHEMATICS);
+                    otherInstanceSteps.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
                 }
 
-                restfuncs.add(POSITION_SHARED);
+                otherInstanceSteps.add(POSITION_SHARED);
 
             }else{
-                restfuncs.add(POSITION_SET);
+                otherInstanceSteps.add(POSITION_SET);
 
                 if(!DRAW_MODE_INDEXED){
-                    restfuncs.add(POSITION_UNINDEX);
+                    otherInstanceSteps.add(POSITION_UNINDEX);
                 }
 
-                restfuncs.add(POSITION_INIT_SCHEMATICS);
+                otherInstanceSteps.add(POSITION_INIT_SCHEMATICS);
 
                 if(CONVERT_POSITIONS_TO_MODELARRAYS){
-                    restfuncs.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
+                    otherInstanceSteps.add(POSITION_CONVERT_POSITIONS_TO_MODEL_MATRIX);
                 }
             }
         }
@@ -135,20 +136,20 @@ public abstract class FSHAssembler implements VLLoggable{
 
     private void configureColors(){
         if(LOAD_COLORS){
-            firstfuncs.add(COLOR_FILE_SET);
+            firstInstanceSteps.add(COLOR_FILE_SET);
 
             if(!DRAW_MODE_INDEXED){
-                firstfuncs.add(COLOR_FILE_LOADED_NONE_INDEXED);
+                firstInstanceSteps.add(COLOR_FILE_LOADED_NONE_INDEXED);
             }
 
             if(INSTANCE_SHARE_COLORS){
-                restfuncs.add(COLOR_SHARED);
+                otherInstanceSteps.add(COLOR_SHARED);
 
             }else{
-                restfuncs.add(COLOR_FILE_SET);
+                otherInstanceSteps.add(COLOR_FILE_SET);
 
                 if(!DRAW_MODE_INDEXED){
-                    restfuncs.add(COLOR_FILE_LOADED_NONE_INDEXED);
+                    otherInstanceSteps.add(COLOR_FILE_LOADED_NONE_INDEXED);
                 }
             }
         }
@@ -156,32 +157,32 @@ public abstract class FSHAssembler implements VLLoggable{
 
     private void configureTexCoords(){
         if(LOAD_TEXCOORDS){
-            firstfuncs.add(TEXTURE_SET);
+            firstInstanceSteps.add(TEXTURE_SET);
 
             if(FLIP_TEXTURE_U){
-                firstfuncs.add(TEXTURE_FLIP_U);
+                firstInstanceSteps.add(TEXTURE_FLIP_U);
             }
             if(FLIP_TEXTURE_V){
-                firstfuncs.add(TEXTURE_FLIP_V);
+                firstInstanceSteps.add(TEXTURE_FLIP_V);
             }
             if(!DRAW_MODE_INDEXED){
-                firstfuncs.add(TEXTURE_UNINDEX);
+                firstInstanceSteps.add(TEXTURE_UNINDEX);
             }
 
             if(INSTANCE_SHARE_TEXCOORDS){
-                restfuncs.add(TEXTURE_SHARED);
+                otherInstanceSteps.add(TEXTURE_SHARED);
 
             }else{
-                restfuncs.add(TEXTURE_SET);
+                otherInstanceSteps.add(TEXTURE_SET);
 
                 if(FLIP_TEXTURE_U){
-                    restfuncs.add(TEXTURE_FLIP_U);
+                    otherInstanceSteps.add(TEXTURE_FLIP_U);
                 }
                 if(FLIP_TEXTURE_V){
-                    restfuncs.add(TEXTURE_FLIP_V);
+                    otherInstanceSteps.add(TEXTURE_FLIP_V);
                 }
                 if(!DRAW_MODE_INDEXED){
-                    restfuncs.add(TEXTURE_UNINDEX);
+                    otherInstanceSteps.add(TEXTURE_UNINDEX);
                 }
             }
         }
@@ -189,20 +190,20 @@ public abstract class FSHAssembler implements VLLoggable{
 
     private void configureNormals(){
         if(LOAD_NORMALS){
-            firstfuncs.add(NORMAL_SET);
+            firstInstanceSteps.add(NORMAL_SET);
 
             if(!DRAW_MODE_INDEXED){
-                firstfuncs.add(NORMAL_UNINDEX);
+                firstInstanceSteps.add(NORMAL_UNINDEX);
             }
 
             if(INSTANCE_SHARE_NORMALS){
-                restfuncs.add(NORMAL_SHARED);
+                otherInstanceSteps.add(NORMAL_SHARED);
 
             }else{
-                restfuncs.add(NORMAL_SET);
+                otherInstanceSteps.add(NORMAL_SET);
 
                 if(!DRAW_MODE_INDEXED){
-                    restfuncs.add(NORMAL_UNINDEX);
+                    otherInstanceSteps.add(NORMAL_UNINDEX);
                 }
             }
         }
@@ -210,13 +211,13 @@ public abstract class FSHAssembler implements VLLoggable{
 
     private void configureIndices(){
         if(LOAD_INDICES){
-            firstfuncs.add(INDICES_SET);
+            firstInstanceSteps.add(INDICES_SET);
 
             if(INSTANCE_SHARE_INDICES){
-                restfuncs.add(INDICES_SHARE);
+                otherInstanceSteps.add(INDICES_SHARE);
 
             }else{
-                restfuncs.add(INDICES_SET);
+                otherInstanceSteps.add(INDICES_SET);
             }
         }
     }
@@ -247,7 +248,7 @@ public abstract class FSHAssembler implements VLLoggable{
         schematics.updateBoundaries();
     }
 
-    protected void unIndexPositions(FSInstance instance){
+    private void unIndexPositions(FSInstance instance){
         short[] indices = instance.indices().provider();
         float[] positions = instance.positions().provider();
         VLListFloat converted = new VLListFloat(positions.length, positions.length / 2);
@@ -265,7 +266,7 @@ public abstract class FSHAssembler implements VLLoggable{
         instance.positions().provider(converted.array());
     }
 
-    protected void unIndexColors(FSInstance instance){
+    private void unIndexColors(FSInstance instance){
         short[] indices = instance.indices().provider();
         float[] colors = instance.colors().provider();
         VLListFloat converted = new VLListFloat(colors.length, colors.length / 2);
@@ -283,7 +284,7 @@ public abstract class FSHAssembler implements VLLoggable{
         instance.colors().provider(converted.array());
     }
 
-    protected void unIndexTexCoords(FSInstance instance){
+    private void unIndexTexCoords(FSInstance instance){
         short[] indices = instance.indices().provider();
         float[] texcoords = instance.texCoords().provider();
         VLListFloat converted = new VLListFloat(texcoords.length, texcoords.length / 2);
@@ -299,7 +300,7 @@ public abstract class FSHAssembler implements VLLoggable{
         instance.texCoords().provider(converted.array());
     }
 
-    protected void unIndexNormals(FSInstance instance){
+    private void unIndexNormals(FSInstance instance){
         short[] indices = instance.indices().provider();
         float[] normals = instance.normals().provider();
         VLListFloat converted = new VLListFloat(normals.length, normals.length / 2);
@@ -317,13 +318,13 @@ public abstract class FSHAssembler implements VLLoggable{
     }
 
     protected final void buildFirst(FSInstance instance, FSHScanner scanner, FSM.Data data){
-        buildTarget(firstfuncs, instance, scanner, data);
-        buildTarget(customfunc, instance, scanner, data);
+        buildTarget(firstInstanceSteps, instance, scanner, data);
+        buildTarget(customInstanceSteps, instance, scanner, data);
     }
 
     protected final void buildRest(FSInstance instance, FSHScanner scanner, FSM.Data data){
-        buildTarget(restfuncs, instance, scanner, data);
-        buildTarget(customfunc, instance, scanner, data);
+        buildTarget(otherInstanceSteps, instance, scanner, data);
+        buildTarget(customInstanceSteps, instance, scanner, data);
     }
 
     private void buildTarget(VLListType<BuildStep> funcs, FSInstance instance, FSHScanner scanner, FSM.Data data){
@@ -337,8 +338,8 @@ public abstract class FSHAssembler implements VLLoggable{
         }
     }
 
-    protected final void checkDebug(){
-        if(firstfuncs.size() == 0){
+    protected void checkDebug(){
+        if(firstInstanceSteps.size() == 0){
             throw new RuntimeException("[ERROR] [Assembler not configured]");
         }
     }
