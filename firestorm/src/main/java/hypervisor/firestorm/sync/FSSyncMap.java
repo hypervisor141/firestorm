571be8b0715ba03c4824056bd0bfbd35a7d28ac5
Nurverek <nurverek@gmail.com>
2021-07-26 12:59:10 +0430

    Converted the fssyncmap to a wrapper of any custom synctype instead to expand it's applications

--- firestorm/src/main/java/hypervisor/firestorm/sync/FSSyncMap.java
+++ firestorm/src/main/java/hypervisor/firestorm/sync/FSSyncMap.java
@@ -4,14 +4,15 @@ import hypervisor.firestorm.engine.FSR;
 import hypervisor.firestorm.engine.FSRTask;
 import hypervisor.vanguard.sync.VLSyncMap;
 import hypervisor.vanguard.sync.VLSyncType;
+import hypervisor.vanguard.utils.VLCopyable;
 
-public abstract class FSSyncMap<SOURCE, TARGET> extends VLSyncMap<SOURCE, TARGET>{
+public abstract class FSSyncMap<SOURCE, TARGET extends VLSyncType<SOURCE>> extends VLSyncMap<SOURCE, TARGET>{
 
     public Post<SOURCE> post;
 
     public FSSyncMap(TARGET target){
         super(target);
-        post = new Post<>(this);
+        post = new Post<>(target);
     }
 
     public FSSyncMap(FSSyncMap<SOURCE, TARGET> src, long flags){
@@ -33,16 +34,33 @@ public abstract class FSSyncMap<SOURCE, TARGET> extends VLSyncMap<SOURCE, TARGET
     @Override
     public void copy(VLSyncType<SOURCE> src, long flags){
         super.copy(src, flags);
-        post = ((FSSyncMap<SOURCE, TARGET>)src).post;
+
+        if((flags & FLAG_REFERENCE) == FLAG_REFERENCE){
+            post = ((FSSyncMap<SOURCE, TARGET>)src).post;
+
+        }else if((flags & FLAG_DUPLICATE) == FLAG_DUPLICATE){
+            post = ((FSSyncMap<SOURCE, TARGET>)src).post;
+
+        }else{
+            Helper.throwMissingAllFlags();
+        }
     }
 
-    public static class Post<SOURCE> implements FSRTask{
+    public static class Post<SOURCE> implements FSRTask, VLCopyable<Post<SOURCE>>{
 
-        public FSSyncMap<SOURCE, ?> map;
+        public VLSyncType<SOURCE> target;
         public SOURCE source;
 
-        public Post(FSSyncMap<SOURCE, ?> map){
-            this.map = map;
+        public Post(VLSyncType<SOURCE> target){
+            this.target = target;
+        }
+
+        public Post(Post<SOURCE> src, long flags){
+            copy(src, flags);
+        }
+
+        protected Post(){
+
         }
 
         public void update(SOURCE source){
@@ -51,7 +69,28 @@ public abstract class FSSyncMap<SOURCE, TARGET> extends VLSyncMap<SOURCE, TARGET
 
         @Override
         public void run(){
-            map.syncPosted(source);
+            target.sync(source);
+        }
+
+        @Override
+        public void copy(Post<SOURCE> src, long flags){
+            Post<SOURCE> target = (Post<SOURCE>)src;
+            source = target.source;
+
+            if((flags & FLAG_REFERENCE) == FLAG_REFERENCE){
+                this.target = target.target;
+
+            }else if((flags & FLAG_DUPLICATE) == FLAG_DUPLICATE){
+                this.target = target.target.duplicate(FLAG_DUPLICATE);
+
+            }else{
+                Helper.throwMissingAllFlags();
+            }
+        }
+
+        @Override
+        public Post<SOURCE> duplicate(long flags){
+            return new Post<>(this, flags);
         }
     }
 }
